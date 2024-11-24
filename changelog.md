e8881f94a6fab0ad72107df3ab3698e36d386803
anshchawla521 <anshchawla521@gmail.com>
2024-11-24 11:03:03 +0530

    Updated ESC schematic

---
+++ changelog.md
@@ -0,0 +1,3 @@
+# Changes
+
+This commit has the latst schematic with standard brushless ESC mosfets , layout is still pedning , size has been kept to minimal so this can be dockedd on top of bb51 board.
--- src/main.cpp
+++ src/main.cpp
@@ -1,175 +1,220 @@
 #include <Arduino.h>
 
-#define deadtime 2      // us
-#define MOTOR1_PMOS PD6 // Pin 6 of ardunio nano
-#define MOTOR1_NMOS PD5 // Pin 5 of arduino nano
-enum output_state { OUTPUT_HIGH = 1, OUTPUT_LOW = 2, OUTPUT_NEUTRAL = 0 };
+#define deadtime 2 // us
+// #define MOTOR1_PMOS PD6 // Pin 6 of ardunio nano
+// #define MOTOR1_NMOS PD5 // Pin 5 of arduino nano
+
+#define MOTOR1_PMOS PB2 // Pin 6 of ardunio nano
+#define MOTOR1_NMOS PB1 // Pin 5 of arduino nano
+enum output_state
+{
+      OUTPUT_HIGH = 1,
+      OUTPUT_LOW = 2,
+      OUTPUT_NEUTRAL = 0
+};
 
 output_state motor1 = OUTPUT_NEUTRAL;
 
 void output_fsm(output_state desired_state);
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  DDRD |=
-      (1 << MOTOR1_PMOS) | (1 << MOTOR1_NMOS); // define Pin 5 and 6 as output
-  output_fsm(OUTPUT_NEUTRAL); // initialise fsm with neutral state
+void setup()
+{
+      // put your setup code here, to run once:
+      Serial.begin(9600);
+      DDRB |=
+          (1 << MOTOR1_PMOS) | (1 << MOTOR1_NMOS); // define Pin 5 and 6 as output
+      output_fsm(OUTPUT_NEUTRAL);                  // initialise fsm with neutral state
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  while (Serial.available() > 0) {
+void loop()
+{
+      // put your main code here, to run repeatedly:
+      while (Serial.available() > 0)
+      {
 
-    char c = Serial.read();
-    if (c == '1') {
-      output_fsm(OUTPUT_HIGH);
-    } else if (c == '0') {
-      output_fsm(OUTPUT_LOW);
-    } else if (c == 'z') {
-      output_fsm(OUTPUT_NEUTRAL);
-    }
-  }
+            char c = Serial.read();
+            if (c == '1')
+            {
+                  output_fsm(OUTPUT_HIGH);
+            }
+            else if (c == '0')
+            {
+                  output_fsm(OUTPUT_LOW);
+            }
+            else if (c == 'z')
+            {
+                  output_fsm(OUTPUT_NEUTRAL);
+            }
+      }
 }
 
-void output_fsm(output_state desired_state) {
-  if (motor1 == OUTPUT_NEUTRAL) {
-    if (desired_state == OUTPUT_NEUTRAL) {
+void output_fsm(output_state desired_state)
+{
+      if (motor1 == OUTPUT_NEUTRAL)
+      {
+            if (desired_state == OUTPUT_NEUTRAL)
+            {
 // turn both MOS OFF
 #ifndef INVERTED_OUTPUTS
-      PORTD |= (1 << MOTOR1_PMOS);  // Pin6 HIGH
-      PORTD &= ~(1 << MOTOR1_NMOS); // Pin5 LOW
+                  PORTB |= (1 << MOTOR1_PMOS);  // Pin6 HIGH
+                  PORTB &= ~(1 << MOTOR1_NMOS); // Pin5 LOW
 #else
-      PORTD &= ~(1 << MOTOR1_PMOS); // Pin6 LOW
-      PORTD |= (1 << MOTOR1_NMOS);  // Pin5 HIGH
+                  PORTB &= ~(1 << MOTOR1_PMOS); // Pin6 LOW
+                  PORTB |= (1 << MOTOR1_NMOS);  // Pin5 HIGH
 #endif
 
-      motor1 = OUTPUT_NEUTRAL;
-    } else if (desired_state == OUTPUT_HIGH) {
+                  motor1 = OUTPUT_NEUTRAL;
+            }
+            else if (desired_state == OUTPUT_HIGH)
+            {
 // turn pmos on
 #ifndef INVERTED_OUTPUTS
-      PORTD &= ~(1 << MOTOR1_NMOS) & ~(1 << MOTOR1_PMOS); // Pin 5 and 6 LOW
+                  PORTB &= ~(1 << MOTOR1_NMOS) & ~(1 << MOTOR1_PMOS); // Pin 5 and 6 LOW
 #else
-      PORTD |= (1 << MOTOR1_NMOS) | (1 << MOTOR1_PMOS); // Pin 5 and 6 HIGH
+                  PORTB |= (1 << MOTOR1_NMOS) | (1 << MOTOR1_PMOS); // Pin 5 and 6 HIGH
 #endif
-      motor1 = OUTPUT_HIGH;
-    } else if (desired_state == OUTPUT_LOW) {
+                  motor1 = OUTPUT_HIGH;
+            }
+            else if (desired_state == OUTPUT_LOW)
+            {
 // turn nmos on
 #ifndef INVERTED_OUTPUTS
-      PORTD |= (1 << MOTOR1_NMOS) | (1 << MOTOR1_PMOS); // Pin 5 and 6 HIGH
+                  PORTB |= (1 << MOTOR1_NMOS) | (1 << MOTOR1_PMOS); // Pin 5 and 6 HIGH
 #else
-      PORTD &= ~(1 << MOTOR1_NMOS) & ~(1 << MOTOR1_PMOS); // Pin 5 and 6 LOW
+                  PORTB &= ~(1 << MOTOR1_NMOS) & ~(1 << MOTOR1_PMOS); // Pin 5 and 6 LOW
 #endif
-      motor1 = OUTPUT_LOW;
-    } else {
+                  motor1 = OUTPUT_LOW;
+            }
+            else
+            {
 #ifndef INVERTED_OUTPUTS
-      PORTD |= (1 << MOTOR1_PMOS);  // Pin6 HIGH
-      PORTD &= ~(1 << MOTOR1_NMOS); // Pin5 LOW
+                  PORTB |= (1 << MOTOR1_PMOS);  // Pin6 HIGH
+                  PORTB &= ~(1 << MOTOR1_NMOS); // Pin5 LOW
 #else
-      PORTD &= ~(1 << MOTOR1_PMOS); // Pin6 LOW
-      PORTD |= (1 << MOTOR1_NMOS);  // Pin5 HIGH
+                  PORTB &= ~(1 << MOTOR1_PMOS); // Pin6 LOW
+                  PORTB |= (1 << MOTOR1_NMOS);  // Pin5 HIGH
 #endif
-      motor1 = OUTPUT_NEUTRAL;
-    }
-  } else if (motor1 == OUTPUT_HIGH) {
-    if (desired_state == OUTPUT_NEUTRAL) {
+                  motor1 = OUTPUT_NEUTRAL;
+            }
+      }
+      else if (motor1 == OUTPUT_HIGH)
+      {
+            if (desired_state == OUTPUT_NEUTRAL)
+            {
 // turn both mos OFF
 #ifndef INVERTED_OUTPUTS
-      PORTD |= (1 << MOTOR1_PMOS);  // Pin6 HIGH
-      PORTD &= ~(1 << MOTOR1_NMOS); // Pin5 LOW
+                  PORTB |= (1 << MOTOR1_PMOS);  // Pin6 HIGH
+                  PORTB &= ~(1 << MOTOR1_NMOS); // Pin5 LOW
 #else
-      PORTD &= ~(1 << MOTOR1_PMOS); // Pin6 LOW
-      PORTD |= (1 << MOTOR1_NMOS);  // Pin5 HIGH
+                  PORTB &= ~(1 << MOTOR1_PMOS); // Pin6 LOW
+                  PORTB |= (1 << MOTOR1_NMOS);  // Pin5 HIGH
 #endif
-      delayMicroseconds(deadtime);
-      motor1 = OUTPUT_NEUTRAL;
-    } else if (desired_state == OUTPUT_HIGH) {
+                  delayMicroseconds(deadtime);
+                  motor1 = OUTPUT_NEUTRAL;
+            }
+            else if (desired_state == OUTPUT_HIGH)
+            {
 // turn pmos on
 #ifndef INVERTED_OUTPUTS
-      PORTD &= ~(1 << MOTOR1_PMOS); // Pin6 LOW
-      PORTD &= ~(1 << MOTOR1_NMOS); // Pin5 LOW
+                  PORTB &= ~(1 << MOTOR1_PMOS); // Pin6 LOW
+                  PORTB &= ~(1 << MOTOR1_NMOS); // Pin5 LOW
 #else
-      PORTD |= (1 << MOTOR1_PMOS); // Pin6 HIGH
-      PORTD |= (1 << MOTOR1_NMOS); // Pin5 HIGH
+                  PORTB |= (1 << MOTOR1_PMOS); // Pin6 HIGH
+                  PORTB |= (1 << MOTOR1_NMOS); // Pin5 HIGH
 #endif
-      motor1 = OUTPUT_HIGH;
-    } else if (desired_state == OUTPUT_LOW) {
+                  motor1 = OUTPUT_HIGH;
+            }
+            else if (desired_state == OUTPUT_LOW)
+            {
 // turn pmos off
 #ifndef INVERTED_OUTPUTS
-      PORTD |= (1 << MOTOR1_PMOS);  // Pin6 HIGH
-      PORTD &= ~(1 << MOTOR1_NMOS); // Pin5 LOW
+                  PORTB |= (1 << MOTOR1_PMOS);  // Pin6 HIGH
+                  PORTB &= ~(1 << MOTOR1_NMOS); // Pin5 LOW
 #else
-      PORTD &= ~(1 << MOTOR1_PMOS); // Pin6 LOW
-      PORTD |= (1 << MOTOR1_NMOS);  // Pin5 HIGH
+                  PORTB &= ~(1 << MOTOR1_PMOS); // Pin6 LOW
+                  PORTB |= (1 << MOTOR1_NMOS);  // Pin5 HIGH
 #endif
-      delayMicroseconds(deadtime);
-      motor1 = OUTPUT_NEUTRAL;
+                  delayMicroseconds(deadtime);
+                  motor1 = OUTPUT_NEUTRAL;
 
-      // now turn on the mos
-      output_fsm(OUTPUT_LOW);
-    } else {
+                  // now turn on the mos
+                  output_fsm(OUTPUT_LOW);
+            }
+            else
+            {
 #ifndef INVERTED_OUTPUTS
-      PORTD |= (1 << MOTOR1_PMOS);  // Pin6 HIGH
-      PORTD &= ~(1 << MOTOR1_NMOS); // Pin5 LOW
+                  PORTB |= (1 << MOTOR1_PMOS);  // Pin6 HIGH
+                  PORTB &= ~(1 << MOTOR1_NMOS); // Pin5 LOW
 #else
-      PORTD &= ~(1 << MOTOR1_PMOS); // Pin6 LOW
-      PORTD |= (1 << MOTOR1_NMOS);  // Pin5 HIGH
+                  PORTB &= ~(1 << MOTOR1_PMOS); // Pin6 LOW
+                  PORTB |= (1 << MOTOR1_NMOS);  // Pin5 HIGH
 #endif
-      motor1 = OUTPUT_NEUTRAL;
-    }
-  } else if (motor1 == OUTPUT_LOW) {
-    if (desired_state == OUTPUT_NEUTRAL) {
+                  motor1 = OUTPUT_NEUTRAL;
+            }
+      }
+      else if (motor1 == OUTPUT_LOW)
+      {
+            if (desired_state == OUTPUT_NEUTRAL)
+            {
 // turn both MOS OFF
 #ifndef INVERTED_OUTPUTS
-      PORTD |= (1 << MOTOR1_PMOS);  // Pin6 HIGH
-      PORTD &= ~(1 << MOTOR1_NMOS); // Pin5 LOW
+                  PORTB |= (1 << MOTOR1_PMOS);  // Pin6 HIGH
+                  PORTB &= ~(1 << MOTOR1_NMOS); // Pin5 LOW
 #else
-      PORTD &= ~(1 << MOTOR1_PMOS); // Pin6 LOW
-      PORTD |= (1 << MOTOR1_NMOS);  // Pin5 HIGH
+                  PORTB &= ~(1 << MOTOR1_PMOS); // Pin6 LOW
+                  PORTB |= (1 << MOTOR1_NMOS);  // Pin5 HIGH
 #endif
-      delayMicroseconds(deadtime);
-      motor1 = OUTPUT_NEUTRAL;
-    } else if (desired_state == OUTPUT_HIGH) {
+                  delayMicroseconds(deadtime);
+                  motor1 = OUTPUT_NEUTRAL;
+            }
+            else if (desired_state == OUTPUT_HIGH)
+            {
 // turn nmos off
 #ifndef INVERTED_OUTPUTS
-      PORTD |= (1 << MOTOR1_PMOS);  // Pin6 HIGH
-      PORTD &= ~(1 << MOTOR1_NMOS); // Pin5 LOW
+                  PORTB |= (1 << MOTOR1_PMOS);  // Pin6 HIGH
+                  PORTB &= ~(1 << MOTOR1_NMOS); // Pin5 LOW
 #else
-      PORTD &= ~(1 << MOTOR1_PMOS); // Pin6 LOW
-      PORTD |= (1 << MOTOR1_NMOS);  // Pin5 HIGH
+                  PORTB &= ~(1 << MOTOR1_PMOS); // Pin6 LOW
+                  PORTB |= (1 << MOTOR1_NMOS);  // Pin5 HIGH
 #endif
-      delayMicroseconds(deadtime);
-      motor1 = OUTPUT_NEUTRAL;
+                  delayMicroseconds(deadtime);
+                  motor1 = OUTPUT_NEUTRAL;
 
-      output_fsm(OUTPUT_HIGH);
-    } else if (desired_state == OUTPUT_LOW) {
+                  output_fsm(OUTPUT_HIGH);
+            }
+            else if (desired_state == OUTPUT_LOW)
+            {
 // turn nmos on
 #ifndef INVERTED_OUTPUTS
-      PORTD |= (1 << MOTOR1_PMOS); // Pin6 HIGH
-      PORTD |= (1 << MOTOR1_NMOS); // Pin5 HIGH
+                  PORTB |= (1 << MOTOR1_PMOS); // Pin6 HIGH
+                  PORTB |= (1 << MOTOR1_NMOS); // Pin5 HIGH
 #else
-      PORTD &= ~(1 << MOTOR1_PMOS); // Pin6 LOW
-      PORTD &= ~(1 << MOTOR1_NMOS); // Pin5 LOW
+                  PORTB &= ~(1 << MOTOR1_PMOS); // Pin6 LOW
+                  PORTB &= ~(1 << MOTOR1_NMOS); // Pin5 LOW
 #endif
-      motor1 = OUTPUT_LOW;
-    } else {
+                  motor1 = OUTPUT_LOW;
+            }
+            else
+            {
 #ifndef INVERTED_OUTPUTS
-      PORTD |= (1 << MOTOR1_PMOS);  // Pin6 HIGH
-      PORTD &= ~(1 << MOTOR1_NMOS); // Pin5 LOW
+                  PORTB |= (1 << MOTOR1_PMOS);  // Pin6 HIGH
+                  PORTB &= ~(1 << MOTOR1_NMOS); // Pin5 LOW
 #else
-      PORTD &= ~(1 << MOTOR1_PMOS); // Pin6 LOW
-      PORTD |= (1 << MOTOR1_NMOS);  // Pin5 HIGH
+                  PORTB &= ~(1 << MOTOR1_PMOS); // Pin6 LOW
+                  PORTB |= (1 << MOTOR1_NMOS);  // Pin5 HIGH
 #endif
-      motor1 = OUTPUT_NEUTRAL;
-    }
-  } else {
+                  motor1 = OUTPUT_NEUTRAL;
+            }
+      }
+      else
+      {
 #ifndef INVERTED_OUTPUTS
-    PORTD |= (1 << MOTOR1_PMOS);  // Pin6 HIGH
-    PORTD &= ~(1 << MOTOR1_NMOS); // Pin5 LOW
+            PORTB |= (1 << MOTOR1_PMOS);  // Pin6 HIGH
+            PORTB &= ~(1 << MOTOR1_NMOS); // Pin5 LOW
 #else
-    PORTD &= ~(1 << MOTOR1_PMOS); // Pin6 LOW
-    PORTD |= (1 << MOTOR1_NMOS);  // Pin5 HIGH
+            PORTB &= ~(1 << MOTOR1_PMOS); // Pin6 LOW
+            PORTB |= (1 << MOTOR1_NMOS);  // Pin5 HIGH
 #endif
-    motor1 = OUTPUT_NEUTRAL;
-  }
+            motor1 = OUTPUT_NEUTRAL;
+      }
 }
